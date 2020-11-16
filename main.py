6dae2fe1649beb06aaca65a477df67b481b42569
Eric Tiller <etiller24@biomedscienceacademy.org>
2020-11-16 15:42:43 +0000

    first draft

--- main.py
+++ main.py
@@ -5,3 +5,16 @@ Period/Core:
 
 
 """
+#Asking the reader for inputs for the story 
+name=input("Enter a name: ")#
+verb=input("Enter a verb: ")#
+adjective=input("Enter a adjective: ")# 
+noun=input("Enter a noun: ")
+verb=input("Enter a verb: ")
+location=input("Enter a location: ")
+food=input("Enter a food:")#
+sport=input("Enter a sport:" )#
+#the base story 
+print("I really like that sport, " + name + ".")
+print(f"{sport} pratice was over finnaly for the week. {name} and their friends {verb} walked to their favorite restruant down the street. {name} sat down and ordered {adjective} {food}. After all the friends ate they all drove {verb} down to {location} and stayed in {name}'s house for the night '") 
+#with a few friends. They had just had [sport] pratice and were all very hungry.  
